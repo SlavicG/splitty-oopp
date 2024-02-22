80b621b0fb2a59b86fa8f32e11bb161f3737e97e
Miruna Coroi <m.e.coroi@student.tudelft.nl>
2024-02-22 18:55:14 +0100

    Added elements on the start page

--- client/src/main/java/client/scenes/StartPageCtrl.java
+++ client/src/main/java/client/scenes/StartPageCtrl.java
@@ -2,14 +2,16 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import javafx.stage.Stage;
 
 public class StartPageCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     @Inject
-    public StartPageCtrl(ServerUtils server, MainCtrl mainCtrl, Stage primaryStage) {
+    public StartPageCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
+    }
+    public void showOverview() {
+        mainCtrl.showOverview();
     }
 }
