ce77e8f0fdef32df5cceb458fa14bf0d6945ec37
Miruna Coroi <m.e.coroi@student.tudelft.nl>
2024-02-23 16:32:49 +0100

    Added elements on the invitations page

--- client/src/main/java/client/scenes/InvitationPageCtrl.java
+++ client/src/main/java/client/scenes/InvitationPageCtrl.java
@@ -2,19 +2,29 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class InvitationPageCtrl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InvitationPageCtrl implements Initializable {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    @FXML
+    private Label code;
     @Inject
     public InvitationPageCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
-    public void f(){
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String s = "ABC7DE";
+        code.setText(s);
     }
-    public void fff(){
-
+    public void overview() {
+        mainCtrl.overviewPage();
     }
 }
