6510012befcfc312a0069367778d3662317f477f
Kinga Kaczmarzyk <K.K.Kaczmarzyk@student.tudelft.nl>
2024-02-27 13:52:09 +0100

    Corrected http file

--- server/src/main/java/server/EventController.java
+++ server/src/main/java/server/EventController.java
@@ -22,27 +22,13 @@ public class EventController {
         this.eventService = eventService;
     }
 
-//    @PostMapping
-//    @ResponseBody
-//    public Event createEvent(@RequestBody Event event) {
-//        System.out.println("Received event: " + event);
-//        return eventService.createEvent(event);
-//    }
-@PostMapping
-@ResponseBody
-public ResponseEntity<?> createEvent(@RequestBody Event event) {
-    try {
+    @PostMapping
+    @ResponseBody
+    public Event createEvent(@RequestBody Event event) {
         System.out.println("Received event: " + event);
-        Event createdEvent = eventService.createEvent(event);
-        return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
-    } catch (Exception e) {
-        // Log the exception details for debugging
-        e.printStackTrace();
-        // Return the actual error message to the client (for debugging purposes only, not for production use)
-        return new ResponseEntity<>("An error occurred while creating the event: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return eventService.createEvent(event);
     }
 
-}
 
 
     @GetMapping("/{id}/users/{user_id}")
