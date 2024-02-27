cedaa8fc23ba12b26fafcccf53f90138e2b39649
Ilker Kilic <i.m.k.kilic@student.tudelft.nl>
2024-02-27 14:53:54 +0100

    Add tests for i18n system

--- client/src/test/java/client/utils/ConfigurationTest.java
+++ client/src/test/java/client/utils/ConfigurationTest.java
@@ -3,6 +3,7 @@ package client.utils;
 import org.junit.jupiter.api.Test;
 
 import java.io.StringReader;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,14 +11,17 @@ public class ConfigurationTest {
 
     @Test
     public void testCustomReader() {
-        Configuration configuration = new Configuration(new StringReader("server.url = this is NOT a url..."));
+        Configuration configuration = new Configuration(new StringReader("server.url = this is NOT a url...\n" +
+                "language.locale = nl"));
         assertEquals("this is NOT a url...", configuration.getServerURL());
+        assertEquals(Locale.forLanguageTag("nl"), configuration.getLocale());
     }
 
     @Test
     public void testDefaultConfig() {
         Configuration configuration = new Configuration();
         assertEquals("http://localhost:8080/", configuration.getServerURL());
+        assertEquals(Locale.forLanguageTag("en"), configuration.getLocale());
     }
 
     @Test
