939ce5da0a58c2b4815ee7af295e483558b03fa1
Miruna Coroi <m.e.coroi@student.tudelft.nl>
2024-03-08 20:16:46 +0100

    Created Tag Class

---
+++ commons/src/main/java/commons/dto/Tag.java
@@ -0,0 +1,52 @@
+package commons.dto;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class Tag {
+    private int id;
+    private String name;
+    private Color color;
+
+    public Tag(int id, String name, Color color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tag tag)) return false;
+        return getId() == tag.getId() && Objects.equals(getName(), tag.getName()) && Objects.equals(getColor(), tag.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor());
+    }
+}
---
+++ commons/src/test/java/commons/dto/TagTest.java
@@ -0,0 +1,52 @@
+package commons.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagTest {
+    private Tag tag1;
+    private Tag tag2;
+    private Color color1;
+    private Color color2;
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
