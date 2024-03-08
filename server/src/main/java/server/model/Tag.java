91e9093704a3a9068275eeb2b58aad6a8536c450
Miruna Coroi <m.e.coroi@student.tudelft.nl>
2024-03-08 20:50:29 +0100

    Created Tag Class in server.model

---
+++ server/src/main/java/server/model/Tag.java
@@ -0,0 +1,72 @@
+package server.model;
+
+import jakarta.persistence.*;
+
+import java.awt.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    private @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) Integer id;
+    private String name;
+    private Color color;
+    @ManyToOne @JoinColumn(name ="event_id") private Event event;
+
+    public Tag(Integer id, String name, Color color, Event event) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.event = event;
+    }
+
+    protected Tag() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tag tag)) return false;
+        return Objects.equals(getId(), tag.getId()) && Objects.equals(getName(), tag.getName()) && Objects.equals(getColor(), tag.getColor()) && Objects.equals(getEvent(), tag.getEvent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getColor(), getEvent());
+    }
+
+}
--- server/src/test/java/server/model/TagTest.java
+++ server/src/test/java/server/model/TagTest.java
@@ -1,4 +1,4 @@
-package commons.dto;
+package server.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,19 +8,22 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TagTest {
-    private Tag tag1;
-    private Tag tag2;
+    private Event event1;
+    private Event event2;
     private Color color1;
     private Color color2;
+    private Tag tag1;
+    private Tag tag2;
 
     @BeforeEach
     void setup() {
         color1 = new Color(255);
         color2 = new Color(0);
-        tag1 = new Tag(1, "food", color1);
-        tag2 = new Tag(2, "travel", color2);
+        event1 = new Event(1, "party", null, null);
+        event2 = new Event(2, "party2", null, null);
+        tag1 = new Tag(1, "food", color1, event1);
+        tag2 = new Tag(2, "travel", color2, event2);
     }
-
     @Test
     void getId() {
         assertEquals(1,tag1.getId());
@@ -34,6 +37,11 @@ class TagTest {
     @Test
     void getColor() {
         assertEquals(color1, tag1.getColor());
+    }
+
+    @Test
+    void getEvent() {
+        assertEquals(event1, tag1.getEvent());
     }
 
     @Test
@@ -53,6 +61,12 @@ class TagTest {
         Color color3 = new Color(2);
         tag1.setColor(color3);
         assertEquals(color3, tag1.getColor());
+    }
+
+    @Test
+    void setEvent() {
+        tag1.setEvent(event2);
+        assertEquals(event2, tag1.getEvent());
     }
 
     @Test
