36fd6ca6f7ec1e7e4f0b73f009acf83b9ce271e4
Veaceslav Guzun <V.Guzun-1@student.tudelft.nl>
2024-03-23 00:40:42 +0100

    Changed considering comments

--- client/src/main/java/client/scenes/AddParticipantCtrl.java
+++ client/src/main/java/client/scenes/AddParticipantCtrl.java
@@ -64,12 +64,12 @@ public class AddParticipantCtrl implements Initializable {
             return;
         }
         if (user == null) {
-            user =
+            User newUser =
                     server.addUsers(new User(null, name.getText(), email.getText(), iban.getText(), bic.getText()));
-            Event changedEvent = server.addUserToEvent(event, event.getId(), user, user.getId());
+            Event changedEvent = server.addUserToEvent(event, event.getId(), newUser, newUser.getId());
             user = null;
         } else {
-            user = server.updateUser(new User(user.getId(),
+            User changedUser = server.updateUser(new User(user.getId(),
                     name.getText(),
                     email.getText(), iban.getText(), bic.getText()), user.getId());
             user = null;
