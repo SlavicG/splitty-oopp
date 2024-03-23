c4e4a70f86271d712b52403bad7a3298e646fbe0
David Gogoana <d.gogoana@student.tudelft.nl>
2024-03-23 02:16:45 +0100

    small fix

--- server/src/main/java/server/admin/AdminController.java
+++ server/src/main/java/server/admin/AdminController.java
@@ -1,10 +1,5 @@
 package server.admin;
-
-import commons.dto.Event;
 import commons.dto.User;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.service.EventService;
 import server.service.UserService;
