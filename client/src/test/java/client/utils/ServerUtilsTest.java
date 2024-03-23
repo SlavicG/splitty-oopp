6c9edaba087481328d0688c0cc779590fc980af0
Veaceslav Guzun <V.Guzun-1@student.tudelft.nl>
2024-03-23 18:17:07 +0100

    Tried to fix issues

--- client/src/test/java/client/utils/ServerUtilsTest.java
+++ client/src/test/java/client/utils/ServerUtilsTest.java
@@ -3,11 +3,11 @@ package client.utils;
 import commons.dto.*;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ServerUtilsTest {
     Configuration configuration = new Configuration();
@@ -19,7 +19,13 @@ class ServerUtilsTest {
     //User user4= server.addUsers(user2);
     @Test
     void constructorTest() {
-        ServerUtils server1 = new ServerUtils(configuration);
+        try {
+            ServerUtils server1 = new ServerUtils(configuration);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Test failed due to an unexpected error: " + e.getMessage());
+        }
+
     }
 
     @Test
