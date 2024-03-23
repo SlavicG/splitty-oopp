b8d20b5a1605ea4583d2cfa2345ca652eb699df0
Abdullah Alpsoy <a.e.alpsoy@student.tudelft.nl>
2024-03-23 03:10:31 +0100

    Update EventTest.java

--- commons/src/test/java/commons/dto/EventTest.java
+++ commons/src/test/java/commons/dto/EventTest.java
@@ -12,13 +12,13 @@ class EventTest {
 
     @Test
     void getId() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         assertEquals(1, event.getId());
     }
 
     @Test
     void setId() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         event.setId(2);
         Event event1 = new Event();
         event1.setId(1);
@@ -28,7 +28,7 @@ class EventTest {
 
     @Test
     void getTitle() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         Event event1 = new Event();
         event1.setId(1);
         assertEquals(1,event1.getId());
@@ -37,7 +37,7 @@ class EventTest {
 
     @Test
     void setTitle() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         event.setTitle("partyZ");
         Event event1 = new Event();
         event1.setTitle("snackbar visit");
@@ -47,13 +47,13 @@ class EventTest {
 
     @Test
     void getUsersIds() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         assertEquals(null, event.getUserIds());
     }
 
     @Test
     void setUserIds() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         List<Integer> userIds = new ArrayList<>();
         userIds.add(5);
         event.setUsers(userIds);
@@ -62,13 +62,13 @@ class EventTest {
 
     @Test
     void getExpenses() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         assertEquals(null, event.getExpenses());
     }
 
     @Test
     void setExpenses() {
-        Event event = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
         List<Expense> expenses = new ArrayList<>();
         LocalDate date = LocalDate.of(2024, 3, 3);
         Expense expense1 = new Expense(1, 20.0, "SnackBar", 3, date, new ArrayList<>(), 1);
@@ -79,8 +79,8 @@ class EventTest {
 
     @Test
     void testEquals() {
-        Event event = new Event(1, "party", null, null);
-        Event event2 = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
+        Event event2 = new Event(1, "party", null, null, null);
         Event event1 = new Event();
         event1.setTitle("party");
         event1.setId(1);
@@ -92,8 +92,8 @@ class EventTest {
 
     @Test
     void testHashCode() {
-        Event event = new Event(1, "party", null, null);
-        Event event2 = new Event(1, "party", null, null);
+        Event event = new Event(1, "party", null, null, null);
+        Event event2 = new Event(1, "party", null, null, null);
         Event event1 = new Event();
         event1.setTitle("party");
         event1.setId(1);
